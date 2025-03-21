9a9738942f4abe022fcd090f90f89bba1b9028be
Muhammed Nihal.K <muhammednihalk4@gmail.com>
2025-03-21 23:47:25 +0530

    Userdashbord (#58)
    * initialization (#1)
    
    * some
    
    * changes
    
    * started
    
    * changes on dashbord
    
    * medical report
    
    * some
    
    * some fixes
    
    * modified home
    
    * some fixes
    
    * chat
    
    * socket change
    
    * chat fixedd
    
    * chat
    
    * overall changes
    
    * some changes
    
    * others
    
    * changes
    
    * splitted
    
    * chart admin
    
    * dashbord
    
    * responsivness
    
    * v
    
    * landing page
    
    * landing page modified
    
    * some fixes
    
    * some fixes
    
    * fixes
    
    * cd
    
    * Loaders added
    
    * Footer
    
    * logo&Loading
    
    * feet: google auth
    
    * token issue
    
    ---------
    
    Co-authored-by: Mohammed Shahid C <ms4811879@gmail.com>

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,16 +1,20 @@
-
 import axios from "axios";
 import Cookies from 'js-cookie';
 
-const token= Cookies.get("accessToken");
+const axiosInstance = axios.create({
 
-console.log(token);
-const axiosInstance=axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
-    withCredentials:true,
-    headers:{
-        Authorization:`Bearer ${token}`
+    withCredentials: true
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    const token = Cookies.get("accessToken");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-})
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export default axiosInstance;
